09d6a97cfa8da925d6aaceb9d9d399ac15ff360a
jvsoest <johan.vansoest@maastro.nl>
2018-09-05 10:23:25 +0200

    Update installDockerFromRoot.sh

--- installDockerFromRoot.sh
+++ installDockerFromRoot.sh
@@ -1,5 +1,5 @@
 #add GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 #add repository to apt-get
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
