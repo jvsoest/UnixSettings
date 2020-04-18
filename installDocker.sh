b846522d9e2f31de9f5f2e996b878e8873f54705
Johan van Soest <info@johanvansoest.nl>
2020-04-18 10:26:09 +0200

    Proper way of installing docker-compose

--- installDocker.sh
+++ installDocker.sh
@@ -5,13 +5,17 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 #update the index of APT
-sudo apt-get update
+sudo apt update
 
 #install docker
-sudo apt-get install -y docker-ce docker-compose
+sudo apt install -y docker-ce
 
 #check running status
 #sudo systemctl status docker
 
 #add current user to the docker group
 sudo usermod -aG docker ${USER}
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
