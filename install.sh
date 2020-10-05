b9038e57d36b97b231f15e6f1f109046157b4cfd
Johan van Soest <info@johanvansoest.nl>
2020-10-05 20:53:43 +0200

    updated to zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 printf "Select your OS:\n1 = Ubuntu (Debian)\n2 = MacOS\nChoice [1,2]: "
 read osChoice
 
+printf "Install pip and pydicom? [Y/n]"
+read pythonChoice
+
 if [ $osChoice == "1" ]; then
     #install git and curl (if needed)
     sudo apt-get install git curl vim python screen tmux
@@ -9,20 +12,13 @@ fi
 
 if [ $osChoice == "2" ]; then
     #install homebrew on macOS
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     brew install tmux
 fi
-
-printf "Install pip and pydicom? [Y/n]"
-read pythonChoice
 
 #install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-#add git integration on command line
-curl -LSso ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-curl -LSso ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
 #install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -33,16 +29,9 @@ ln -s ~/Repositories/UnixSettings/vim/.vimrc ~/.vimrc
 #install plugins configured in .vimrc
 vim +PluginInstall +qall
 
-#set bash settings (git integration)
-if [ $osChoice == "1" ]; then
-    ln -s ~/Repositories/UnixSettings/bash/.bashrc_ubuntu ~/.bashrc
-fi
-if [ $osChoice == "2" ]; then
-    ln -s ~/Repositories/UnixSettings/bash/.bashrc ~/.bashrc
-fi
-ln -s ~/Repositories/UnixSettings/bash/.screenrc ~/.screenrc
+mv ~/.zshrc ~/.zshrc_backup
+ln -s ~/Repositories/UnixSettings/bash/.zshrc ~/.zshrc
 ln -s ~/Repositories/UnixSettings/tmux.conf ~/.tmux.conf
-ln -s ~/Repositories/UnixSettings/bash/.bash_profile ~/.bash_profile
 
 #add scripts folder to path
 chmod -R +x ~/Repositories/UnixSettings/scripts
