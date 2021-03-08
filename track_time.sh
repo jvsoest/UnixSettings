be40e7e23b5892e4419ffc604a1772762fa98163
Johan van Soest <info@johanvansoest.nl>
2021-03-08 16:59:49 +0100

    Updated files for removal and added time tracking shell script

---
+++ track_time.sh
@@ -0,0 +1,1 @@
+echo "<http://$(hostname)/time/timePoint/$(uuidgen)> <http://jvsoest.eu/ontology/timeTrack.owl#at_time> \"$(date '+%Y-%m-%dT%T')\"^^<http://www.w3.org/2001/XMLSchema#dateTime>; <http://jvsoest.eu/ontology/timeTrack.owl#hostname> <http://$(hostname)>." >> /mnt/c/Users/johan/Desktop/timeTrack_$(date '+%Y%m%d').ttl
