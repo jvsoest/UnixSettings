a524988aebb34b2c52fb5b0acb4ce1bb619fc483
Johan van Soest <info@johanvansoest.nl>
2021-03-08 17:02:47 +0100

    Added external folder for results

--- track_time.sh
+++ track_time.sh
@@ -1,1 +1,3 @@
-echo "<http://$(hostname)/time/timePoint/$(uuidgen)> <http://jvsoest.eu/ontology/timeTrack.owl#at_time> \"$(date '+%Y-%m-%dT%T')\"^^<http://www.w3.org/2001/XMLSchema#dateTime>; <http://jvsoest.eu/ontology/timeTrack.owl#hostname> <http://$(hostname)>." >> /mnt/c/Users/johan/Desktop/timeTrack_$(date '+%Y%m%d').ttl
+mkdir -p ~/.track_time
+
+echo "<http://$(hostname)/time/timePoint/$(uuidgen)> <http://jvsoest.eu/ontology/timeTrack.owl#at_time> \"$(date '+%Y-%m-%dT%T')\"^^<http://www.w3.org/2001/XMLSchema#dateTime>; <http://jvsoest.eu/ontology/timeTrack.owl#hostname> <http://$(hostname)>." >> ~/.track_time/$(date '+%Y%m%d').ttl
