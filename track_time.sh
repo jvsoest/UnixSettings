b5af0fbbc087a3f9c05d6e9a63c35609687d0963
Johan van Soest <info@johanvansoest.nl>
2021-03-22 12:08:30 +0100

    Using small hostnames

--- track_time.sh
+++ track_time.sh
@@ -1,3 +1,3 @@
 mkdir -p $HOME/.track_time
 
-echo "<http://$(hostname)/track_time/timePoint/$(uuidgen)> <http://jvsoest.eu/ontology/timeTrack.owl#at_time> \"$(date '+%Y-%m-%dT%T')\"^^<http://www.w3.org/2001/XMLSchema#dateTime>; <http://jvsoest.eu/ontology/timeTrack.owl#hostname> <http://$(hostname)>; <http://jvsoest.eu/ontology/timeTrack.owl#category> <http://$(hostname)/track_time/category/$(cat $HOME/.track_time/category.txt)>." >> $HOME/.track_time/$(date '+%Y%m%d').ttl
+echo "<http://$(hostname -s)/track_time/timePoint/$(uuidgen)> <http://jvsoest.eu/ontology/timeTrack.owl#at_time> \"$(date '+%Y-%m-%dT%T')\"^^<http://www.w3.org/2001/XMLSchema#dateTime>; <http://jvsoest.eu/ontology/timeTrack.owl#hostname> <http://$(hostname -s)>; <http://jvsoest.eu/ontology/timeTrack.owl#category> <http://$(hostname -s)/track_time/category/$(cat $HOME/.track_time/category.txt)>." >> $HOME/.track_time/$(date '+%Y%m%d').ttl
