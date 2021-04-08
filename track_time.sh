ea9f95ab8fc792ae232dbc8a18e45bb758e2bdc3
Johan van Soest <info@johanvansoest.nl>
2021-04-08 15:42:53 +0200

    Updated to use multiple categories (e.g. for projects and activities)

--- track_time.sh
+++ track_time.sh
@@ -1,3 +1,14 @@
 mkdir -p $HOME/.track_time
 
-echo "<http://$(hostname -s)/track_time/timePoint/$(uuidgen)> <http://jvsoest.eu/ontology/timeTrack.owl#at_time> \"$(date '+%Y-%m-%dT%T')\"^^<http://www.w3.org/2001/XMLSchema#dateTime>; <http://jvsoest.eu/ontology/timeTrack.owl#hostname> <http://$(hostname -s)>; <http://jvsoest.eu/ontology/timeTrack.owl#category> <http://$(hostname -s)/track_time/category/$(cat $HOME/.track_time/category.txt)>." >> $HOME/.track_time/$(date '+%Y%m%d').ttl
+categories=$(cat $HOME/.track_time/category.txt)
+categoryUrlString=""
+for i in $(echo $categories | tr ";" "\n")
+do
+    categoryUrlString=$categoryUrlString",<http://$(hostname -s)/track_time/category/$i>"
+done
+
+categoryUrlString=${categoryUrlString:1}
+
+echo $categoryUrlString
+
+echo "<http://$(hostname -s)/track_time/timePoint/$(uuidgen)> <http://jvsoest.eu/ontology/timeTrack.owl#at_time> \"$(date '+%Y-%m-%dT%T')\"^^<http://www.w3.org/2001/XMLSchema#dateTime>; <http://jvsoest.eu/ontology/timeTrack.owl#hostname> <http://$(hostname -s)>; <http://jvsoest.eu/ontology/timeTrack.owl#category> $categoryUrlString." >> $HOME/.track_time/$(date '+%Y%m%d').ttl
